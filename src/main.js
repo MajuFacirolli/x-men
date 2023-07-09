7582a413469c6c1528661163892ea28345558f9a
Maju Facirolli <majufacirolli2004@hotmail.com>
2023-07-09 18:37:45 -0300

    adicionando funções de mudança de persongem

---
+++ src/main.js
@@ -0,0 +1,40 @@
+const personagens = document.querySelectorAll('.personagem');
+personagens.forEach((personagem) => {
+    personagem.addEventListener('click', () => {
+        
+        if(window.innerWidth < 450) {
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+
+        removerSelecaoPersonagem();
+
+        personagem.classList.add('selecionado');
+
+        alterarImagemPersonagemSelecionado(personagem);
+
+        alterarNomePersonagemSelecionado(personagem);
+
+        alterarDescricaoPersonagemSelecionado(personagem);
+    })
+})
+
+function removerSelecaoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `src/card-${idPersonagem}.png`;
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
